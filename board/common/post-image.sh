520c998aedd4c6ec39c43ac8e89e8b520f58bdb1
Joachim Nilsson <troglobit@gmail.com>
2020-04-28 16:09:06 +0200

    Mangle resulting image name -> output/images/netbox-$plf.img
    By default, builds are calle netbox-$platform.img, unless we are
    building a release, in which case they also have -$version suffix.
    
    If TFTPDIR is set, let scp copy the resulting image there.
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

---
+++ board/common/post-image.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+rc=0
+plf=`echo $BR2_DEFCONFIG | sed 's/.*_\(.*\)_defconfig.*$/\1/'`
+
+if [ -n "$RELEASE" ]; then
+    ver=`$BR2_EXTERNAL/bin/mkversion -f $BR2_EXTERNAL`
+    img=$BINARIES_DIR/$BR2_EXTERNAL_ID-$plf-$ver.img
+
+    if [ "$RELEASE" != "$ver" ]; then
+       echo "==============================================================================="
+       echo "WARNING: Release verision '$RELEASE' does not match tag '$ver'!"
+       echo "==============================================================================="
+       rc=1
+    fi
+else
+    img=$BINARIES_DIR/$BR2_EXTERNAL_ID-$plf.img
+fi
+
+mv -v $BINARIES_DIR/rootfs.squashfs $img
+
+# Set TFTPDIR, in your .bashrc, or similar, to copy the resulting image
+# to your FTP/TFTP server directory.  Notice the use of scp, so you can
+# copy the image to another system.
+if [ -n "$TFTPDIR" ]; then
+    echo "xfering '$img' -> '$TFTPDIR/$fn'"
+    scp -B $img $TFTPDIR
+fi
+
+exit $rc
