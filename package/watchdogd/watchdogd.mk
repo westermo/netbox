cbc45a6317a6078c93e56b95a398b2fee9951ab7
Joachim Wiberg <troglobit@gmail.com>
2020-09-05 19:59:07 +0200

    package/watchdogd: refactor init script and fix script paths
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/watchdogd/watchdogd.mk
+++ package/watchdogd/watchdogd.mk
@@ -12,12 +12,12 @@ WATCHDOGD_LICENSE_FILES = LICENSE
 WATCHDOGD_DEPENDENCIES = host-pkgconf libconfuse libite libuev
 
 define WATCHDOGD_INSTALL_INIT_SYSTEMD
-	$(INSTALL) -D -m 0644 package/watchdogd/watchdogd.service \
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_NETBOX_PATH)/package/watchdogd/watchdogd.service \
 		$(TARGET_DIR)/usr/lib/systemd/system/watchdogd.service
 endef
 
 define WATCHDOGD_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 0755 package/watchdogd/S01watchdogd \
+	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_NETBOX_PATH)/package/watchdogd/S01watchdogd \
 		$(TARGET_DIR)/etc/init.d/S01watchdogd
 endef
 
