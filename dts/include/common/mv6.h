1e4f10236b121aababf936b8f9dd3139d81b2b61
Tobias Waldekranz <tobias@waldekranz.com>
2020-09-13 20:54:38 +0200

    dts/envoy: Add Envious board

--- dts/include/common/mv6.h
+++ dts/include/common/mv6.h
@@ -1,11 +1,21 @@
 
+#define _mv6_port_common(_reg, _label, _mode)		\
+	reg = <_reg>;					\
+	label = _label;					\
+	local-mac-address = [00 00 00 00 00 00];	\
+	phy-mode = _mode
+
 #define mv6_port(_reg, _label, _phy, _mode)		 \
 	port@_reg {					 \
-		reg = <_reg>;				 \
-		label = _label;				 \
-		local-mac-address = [00 00 00 00 00 00]; \
 		phy-handle = <_phy>;			 \
-		phy-mode = _mode;			 \
+		_mv6_port_common(_reg, _label, _mode);	 \
+	}
+
+#define mv6_port_sfp(_reg, _label, _sfp, _mode)		 \
+	port@_reg {					 \
+		managed = "in-band-status";		 \
+		sfp = <_sfp>;				 \
+		_mv6_port_common(_reg, _label, _mode);	 \
 	}
 
 #define mv6_phy(_reg)					\
