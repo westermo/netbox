b1635c5fd7185a865a03886751056f7a87268d97
Joachim Wiberg <troglobit@gmail.com>
2020-09-17 21:26:15 +0200

    package/sysklogd2: Local update of sysklogd to v2
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ package/sysklogd2/sysklogd2.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+# sysklogd
+#
+################################################################################
+
+SYSKLOGD2_VERSION = 2.1.2
+SYSKLOGD2_SOURCE = sysklogd-$(SYSKLOGD2_VERSION).tar.gz
+SYSKLOGD2_SITE = https://github.com/troglobit/sysklogd/releases/download/v$(SYSKLOGD2_VERSION)
+SYSKLOGD2_LICENSE = BSD-3-Clause
+
+define SYSKLOGD2_INSTALL_SAMPLE_CONFIG
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_NETBOX_PATH)/package/sysklogd2/syslog.conf \
+                $(TARGET_DIR)/etc/syslog.conf
+endef
+
+SYSKLOGD2_POST_INSTALL_TARGET_HOOKS += SYSKLOGD2_INSTALL_SAMPLE_CONFIG
+
+define SYSKLOGD2_INSTALL_FINIT_SVC
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_NETBOX_PATH)/package/sysklogd2/sysklogd.svc \
+		$(TARGET_DIR)/etc/finit.d/available/sysklogd.conf
+	ln -sf /etc/finit.d/available/sysklogd.conf $(FINIT_D)/enabled/sysklogd.conf
+endef
+
+SYSKLOGD2_POST_INSTALL_TARGET_HOOKS += SYSKLOGD2_INSTALL_FINIT_SVC
+
+$(eval $(autotools-package))
