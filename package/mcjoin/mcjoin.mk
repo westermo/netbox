9981aac4095235af2932868e095f3f47fbeec6d6
Tobias Waldekranz <tobias@waldekranz.com>
2021-01-04 17:54:25 +0100

    package/mcjoin: new package
    Signed-off-by: Tobias Waldekranz <tobias@waldekranz.com>
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ package/mcjoin/mcjoin.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# mcjoin
+#
+################################################################################
+
+MCJOIN_VERSION = 2.7
+MCJOIN_SOURCE  = mcjoin-$(MCJOIN_VERSION).tar.gz
+MCJOIN_SITE    = https://github.com/troglobit/mcjoin/releases/download/v$(MCJOIN_VERSION)
+MCJOIN_LICENSE = ISC
+MCJOIN_LICENSE_FILES = LICENSE
+
+$(eval $(autotools-package))
