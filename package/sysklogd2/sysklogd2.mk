0ffbd6e7f34729924c81fbdfbe341c8e3ac1072a
Joachim Wiberg <troglobit@gmail.com>
2021-01-15 12:41:49 +0100

    package/sysklogd2: bump version to v2.2.0
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/sysklogd2/sysklogd2.mk
+++ package/sysklogd2/sysklogd2.mk
@@ -4,17 +4,17 @@
 #
 ################################################################################
 
-#SYSKLOGD2_VERSION = 2.1.2
-#SYSKLOGD2_SOURCE = sysklogd-$(SYSKLOGD2_VERSION).tar.gz
-#SYSKLOGD2_SITE = https://github.com/troglobit/sysklogd/releases/download/v$(SYSKLOGD2_VERSION)
+SYSKLOGD2_VERSION = 2.2.0
+SYSKLOGD2_SOURCE = sysklogd-$(SYSKLOGD2_VERSION).tar.gz
+SYSKLOGD2_SITE = https://github.com/troglobit/sysklogd/releases/download/v$(SYSKLOGD2_VERSION)
 SYSKLOGD2_LICENSE = BSD-3-Clause
 SYSKLOGD2_CONF_OPTS = --exec-prefix=/ --disable-silent-rules
 
 # Create configure script using autoreconf when building from git
-SYSKLOGD2_VERSION = a95c4ba
-SYSKLOGD2_SITE = git://github.com/troglobit/sysklogd.git
-SYSKLOGD2_AUTORECONF = YES
-SYSKLOGD2_DEPENDENCIES += host-automake host-autoconf host-libtool
+#SYSKLOGD2_VERSION = a95c4ba
+#SYSKLOGD2_SITE = git://github.com/troglobit/sysklogd.git
+#SYSKLOGD2_AUTORECONF = YES
+#SYSKLOGD2_DEPENDENCIES += host-automake host-autoconf host-libtool
 
 define SYSKLOGD2_INSTALL_SAMPLE_CONFIG
 	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_NETBOX_PATH)/package/sysklogd2/syslog.conf \
