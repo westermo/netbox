29d35fd995ffe470227a17dd6a71f6a8fe39849c
Joachim Wiberg <troglobit@gmail.com>
2021-02-09 22:09:20 +0100

    package/sysklogd2: Finit service file fixes
    - Use relative symlink to available/ directory
    - Drop absolute path to binary¹
    ___
    ¹ this requires changing all other services that depend on syslogd!
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/sysklogd2/sysklogd2.mk
+++ package/sysklogd2/sysklogd2.mk
@@ -26,7 +26,7 @@ SYSKLOGD2_POST_INSTALL_TARGET_HOOKS += SYSKLOGD2_INSTALL_SAMPLE_CONFIG
 define SYSKLOGD2_INSTALL_FINIT_SVC
 	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_NETBOX_PATH)/package/sysklogd2/sysklogd.svc \
 		$(TARGET_DIR)/etc/finit.d/available/sysklogd.conf
-	ln -sf /etc/finit.d/available/sysklogd.conf $(FINIT_D)/enabled/sysklogd.conf
+	ln -sf ../available/sysklogd.conf $(FINIT_D)/enabled/sysklogd.conf
 endef
 
 SYSKLOGD2_POST_INSTALL_TARGET_HOOKS += SYSKLOGD2_INSTALL_FINIT_SVC
