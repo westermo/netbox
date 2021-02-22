5c403061814208819900298907775662393e8269
Joachim Wiberg <troglobit@gmail.com>
2021-02-22 07:34:09 +0100

    package/finit: bump to latest finit git hash, fc82f02
    Stable and working condtions on NetBox, Debian, and Alpine Linux.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/finit/finit.mk
+++ package/finit/finit.mk
@@ -14,7 +14,7 @@ FINIT_DEPENDENCIES = host-pkgconf libite libuev
 FINIT_D = $(TARGET_DIR)/etc/finit.d
 
 # Create configure script using autoreconf when building from git
-FINIT_VERSION = 8266439
+FINIT_VERSION = fc82f02
 FINIT_SITE = git://github.com/troglobit/finit.git
 FINIT_AUTORECONF = YES
 FINIT_DEPENDENCIES += host-automake host-autoconf host-libtool
