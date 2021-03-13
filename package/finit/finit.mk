8ae484e837807f0c5718339fc68d446836b85e5d
Joachim Wiberg <troglobit@gmail.com>
2021-03-13 21:46:32 +0100

    package/finit: disable fallback shell (security)
    Let x11 plugin depend on the X11r7 package(s).
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/finit/finit.mk
+++ package/finit/finit.mk
@@ -25,16 +25,16 @@ FINIT_D = $(TARGET_DIR)/etc/finit.d
 # /lib/finit/ and scripts in /libexec, both are set by --exec-prefix.
 # The localstatedir is set to the correct system path by Buildroot, so
 # no override necessary there.
+#	--enable-fallback-shell
 FINIT_CONF_OPTS =				\
 	--prefix=/usr				\
 	--exec-prefix=/				\
 	--disable-doc				\
 	--disable-contrib			\
-	--enable-fallback-shell			\
-	--enable-x11-common-plugin		\
 	$(if $(SKELETON_INIT_COMMON_ISSUE),--with-heading="$(SKELETON_INIT_COMMON_ISSUE)") \
 	$(if $(SKELETON_INIT_COMMON_HOSTNAME),--with-hostname="$(SKELETON_INIT_COMMON_HOSTNAME)") \
 	$(if $(BR2_PACKAGE_ALSA_UTILS),--enable-alsa-utils-plugin) \
-	$(if $(BR2_PACKAGE_DBUS),--enable-dbus-plugin)
+	$(if $(BR2_PACKAGE_DBUS),--enable-dbus-plugin) \
+	$(if $(BR2_PACKAGE_XORG7),--enable-x11-common-plugin)
 
 $(eval $(autotools-package))
