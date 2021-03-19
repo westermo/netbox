5608fa20fc92cd601e8dfa6908ddefbe9d273347
Joachim Wiberg <troglobit@gmail.com>
2021-03-19 10:08:37 +0100

    package/finit: bump version to v4.0-rc3
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/finit/finit.mk
+++ package/finit/finit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FINIT_VERSION = 4.0-rc2
+FINIT_VERSION = 4.0-rc3
 FINIT_SOURCE = finit-$(FINIT_VERSION).tar.gz
 FINIT_SITE = https://github.com/troglobit/finit/releases/download/$(FINIT_VERSION)
 FINIT_LICENSE = MIT
