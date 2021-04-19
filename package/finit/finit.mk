2a35d7ea5c8a56a0467aeda69a6ad1662236dd31
Joachim Wiberg <troglobit@gmail.com>
2021-04-19 15:04:53 +0200

    packages/finit: bump to latest GIT hash before final 4.0 release
     - fallback shell now completely removed, drop comment
     - Empty exec prefix to prevent leading // in paths
     - Enable built-in watchdog service by default.  The hand-over
       to watchdogd has been throughly tested
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/finit/finit.mk
+++ package/finit/finit.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-FINIT_VERSION = 4.0-rc3
-FINIT_SOURCE = finit-$(FINIT_VERSION).tar.gz
-FINIT_SITE = https://github.com/troglobit/finit/releases/download/$(FINIT_VERSION)
+#FINIT_VERSION = 4.0-rc3
+#FINIT_SOURCE = finit-$(FINIT_VERSION).tar.gz
+#FINIT_SITE = https://github.com/troglobit/finit/releases/download/$(FINIT_VERSION)
 FINIT_LICENSE = MIT
 FINIT_LICENSE_FILES = LICENSE
 FINIT_INSTALL_STAGING = YES
@@ -14,10 +14,10 @@ FINIT_DEPENDENCIES = host-pkgconf libite libuev
 FINIT_D = $(TARGET_DIR)/etc/finit.d
 
 # Create configure script using autoreconf when building from git
-#FINIT_VERSION = 460add3
-#FINIT_SITE = git://github.com/troglobit/finit.git
-#FINIT_AUTORECONF = YES
-#FINIT_DEPENDENCIES += host-automake host-autoconf host-libtool
+FINIT_VERSION = 099672f
+FINIT_SITE = git://github.com/troglobit/finit.git
+FINIT_AUTORECONF = YES
+FINIT_DEPENDENCIES += host-automake host-autoconf host-libtool
 
 # Buildroot defaults to /usr for both prefix and exec-prefix, this we
 # must override because we want to install into /sbin and /bin for the
@@ -25,12 +25,12 @@ FINIT_D = $(TARGET_DIR)/etc/finit.d
 # /lib/finit/ and scripts in /libexec, both are set by --exec-prefix.
 # The localstatedir is set to the correct system path by Buildroot, so
 # no override necessary there.
-#	--enable-fallback-shell
 FINIT_CONF_OPTS =				\
 	--prefix=/usr				\
-	--exec-prefix=/				\
+	--exec-prefix=				\
 	--disable-doc				\
 	--disable-contrib			\
+	--with-watchdog				\
 	$(if $(SKELETON_INIT_COMMON_ISSUE),--with-heading="$(SKELETON_INIT_COMMON_ISSUE)") \
 	$(if $(SKELETON_INIT_COMMON_HOSTNAME),--with-hostname="$(SKELETON_INIT_COMMON_HOSTNAME)") \
 	$(if $(BR2_PACKAGE_ALSA_UTILS),--enable-alsa-utils-plugin) \
