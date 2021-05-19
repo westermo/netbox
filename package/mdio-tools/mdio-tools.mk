6b8330e6671041a85e7dd2f1d634083c324dc542
Tobias Waldekranz <tobias@waldekranz.com>
2021-05-19 16:01:22 +0200

    package/mdio-tools: Use actually existing archive
    Previous URL was bogus; you had to set a local override to actually
    build it.

--- package/mdio-tools/mdio-tools.mk
+++ package/mdio-tools/mdio-tools.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-MDIO_TOOLS_VERSION = 1.0.0
-MDIO_TOOLS_SOURCE  = mdio-tools-$(MDIO_TOOLS_VERSION).tar.xz
-MDIO_TOOLS_SITE    = https://github.com/wkz/mdio-tools/releases/download/$(MDIO_TOOLS_VERSION)
+MDIO_TOOLS_VERSION = 1.0.0-beta1
+MDIO_TOOLS_SOURCE  = $(MDIO_TOOLS_VERSION).tar.gz
+MDIO_TOOLS_SITE    = https://github.com/wkz/mdio-tools/archive/refs/tags
 MDIO_TOOLS_LICENSE = GPL-2.0
 MDIO_TOOLS_LICENSE_FILES = COPYING
 MDIO_TOOLS_AUTORECONF = YES
