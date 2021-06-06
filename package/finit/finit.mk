42a05a2ef8c6af7074ae889d330c1f5f90d0b523
Joachim Wiberg <troglobit@gmail.com>
2021-06-06 17:58:38 +0200

    package/finit: bump version to v4.1
    - Bump version, update SHA256 hash
    - Refactor conditionals using recommended upstream format
    - Drop --with-heading, probed from /etc/os-release now
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- package/finit/finit.mk
+++ package/finit/finit.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FINIT_VERSION = 4.0
+FINIT_VERSION = 4.1
 FINIT_SOURCE = finit-$(FINIT_VERSION).tar.gz
 FINIT_SITE = https://github.com/troglobit/finit/releases/download/$(FINIT_VERSION)
 FINIT_LICENSE = MIT
@@ -30,11 +30,30 @@ FINIT_CONF_OPTS =				\
 	--exec-prefix=				\
 	--disable-doc				\
 	--disable-contrib			\
-	--with-watchdog				\
-	$(if $(SKELETON_INIT_COMMON_ISSUE),--with-heading="$(SKELETON_INIT_COMMON_ISSUE)") \
-	$(if $(SKELETON_INIT_COMMON_HOSTNAME),--with-hostname="$(SKELETON_INIT_COMMON_HOSTNAME)") \
-	$(if $(BR2_PACKAGE_ALSA_UTILS),--enable-alsa-utils-plugin) \
-	$(if $(BR2_PACKAGE_DBUS),--enable-dbus-plugin) \
-	$(if $(BR2_PACKAGE_XORG7),--enable-x11-common-plugin)
+	--with-watchdog
+
+# Disable/Enable features
+ifneq ($(SKELETON_INIT_COMMON_HOSTNAME),)
+FINIT_CONF_OPTS += --with-hostname="$(SKELETON_INIT_COMMON_HOSTNAME)"
+endif
+
+# Disable/Enable features depending on other packages
+ifeq ($(BR2_PACKAGE_ALSA_UTILS),y)
+FINIT_CONF_OPTS += --enable-alsa-utils-plugin
+else
+FINIT_CONF_OPTS += --disable-alsa-utils-plugin
+endif
+
+ifeq ($(BR2_PACKAGE_DBUS),y)
+FINIT_CONF_OPTS += --enable-dbus-plugin
+else
+FINIT_CONF_OPTS += --disable-dbus-plugin
+endif
+
+ifeq ($(BR2_PACKAGE_XORG7),y)
+FINIT_CONF_OPTS += --enable-x11-common-plugin
+else
+FINIT_CONF_OPTS += --disable-x11-common-plugin
+endif
 
 $(eval $(autotools-package))
