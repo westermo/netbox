5519d7268eb2e2ed042d3f930f66d54fe2218777
Tobias Waldekranz <tobias@waldekranz.com>
2021-09-19 00:24:20 +0200

    package/mdio-tools: Update to 1.0.0
    Now that we have a proper release tarball, skip autoreconf.

--- package/mdio-tools/mdio-tools.mk
+++ package/mdio-tools/mdio-tools.mk
@@ -4,12 +4,12 @@
 #
 ################################################################################
 
-MDIO_TOOLS_VERSION = 1.0.0-beta1
-MDIO_TOOLS_SOURCE  = $(MDIO_TOOLS_VERSION).tar.gz
-MDIO_TOOLS_SITE    = https://github.com/wkz/mdio-tools/archive/refs/tags
+MDIO_TOOLS_VERSION = 1.0.0
+MDIO_TOOLS_SOURCE  = mdio-tools-$(MDIO_TOOLS_VERSION).tar.gz
+MDIO_TOOLS_SITE    = https://github.com/wkz/mdio-tools/releases/download/$(MDIO_TOOLS_VERSION)
 MDIO_TOOLS_LICENSE = GPL-2.0
 MDIO_TOOLS_LICENSE_FILES = COPYING
-MDIO_TOOLS_AUTORECONF = YES
+MDIO_TOOLS_INSTALL_STAGING = YES
 
 MDIO_TOOLS_MODULE_SUBDIRS = kernel
 MDIO_TOOLS_MODULE_MAKE_OPTS = KDIR=$(LINUX_DIR)
