35e911f228a7f95913895da9ba5b0441d962a78d
Joachim Wiberg <troglobit@gmail.com>
2021-09-22 18:56:28 +0200

    package/brist: new package
    Brist is a framework for testing the Linux bridge.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

---
+++ package/brist/brist.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# brist
+#
+################################################################################
+
+BRIST_VERSION = 1.0
+BRIST_SOURCE = brist-$(BRIST_VERSION).tar.gz
+BRIST_SITE = https://github.com/westermo/brist/releases/download/$(BRIST_VERSION)
+BRIST_LICENSE = MIT
+BRIST_LICENSE_FILES = LICENSE
+
+define BRIST_INSTALL_TARGET_CMDS
+	$(TARGET_MAKE_ENV) $(MAKE) -C $(@D) prefix=/usr DESTDIR="$(TARGET_DIR)" install
+	chmod +x $(TARGET_DIR)/usr/lib/brist/brist.sh
+endef
+
+$(eval $(generic-package))
