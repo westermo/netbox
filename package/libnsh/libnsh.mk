14d774fd3f5c784c7d8b54fcef569a32837563ea
Andy Koszela <andy@ehostunrea.ch>
2021-12-09 15:49:27 +0100

    package/libnsh: new package
    This patch adds support for libnsh, a Net-SNMP helper library needed by other
    Westermo projects that build on NetBox.
    
    Signed-off-by: Andy Koszela <andy.koszela@westermo.com>

---
+++ package/libnsh/libnsh.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# libnsh
+#
+################################################################################
+
+LIBNSH_VERSION = 0.4
+LIBNSH_PKGNAME = libnsh
+LIBNSH_SITE = https://github.com/westermo/$(LIBNSH_PKGNAME)/releases/download/v$(LIBNSH_VERSION)
+LIBNSH_INSTALL_STAGING = YES
+LIBNSH_LICENSE = MIT
+LIBNSH_LICENSE_FILES = LICENSE
+LIBNSH_DEPENDENCIES = host-pkgconf netsnmp
+
+$(eval $(autotools-package))
