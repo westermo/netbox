0bf0fb94f688c8cb764fe709eddeb82ce3b97f24
Andy Koszela <andy.koszela@westermo.com>
2021-12-09 13:34:36 +0100

    Post-review cleanup
    Signed-off-by: Andy Koszela <andy.koszela@westermo.com>

--- package/libnsh/libnsh.mk
+++ package/libnsh/libnsh.mk
@@ -6,14 +6,10 @@
 
 LIBNSH_VERSION = 0.4
 LIBNSH_PKGNAME = libnsh
-LIBNSH_SOURCE = $(LIBNSH_PKGNAME)-$(LIBNSH_VERSION).tar.gz
-# This looks like a typo, but the 'v' needs to be there
 LIBNSH_SITE = https://github.com/westermo/$(LIBNSH_PKGNAME)/releases/download/v$(LIBNSH_VERSION)
 LIBNSH_INSTALL_STAGING = YES
-LIBNSH_AUTORECONF = YES
 LIBNSH_LICENSE = MIT
 LIBNSH_LICENSE_FILES = LICENSE
-#LIBNSH_CONF_OPTS =
 LIBNSH_DEPENDENCIES = host-pkgconf netsnmp
 
 $(eval $(autotools-package))
