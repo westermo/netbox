6bf6ff7dc34131b294b4d52a3d33e319413104a6
Tobias Waldekranz <tobias@waldekranz.com>
2022-05-04 13:53:36 +0200

    package/mdio-tools: Update to 1.1.0

--- package/mdio-tools/mdio-tools.mk
+++ package/mdio-tools/mdio-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MDIO_TOOLS_VERSION = 1.0.0
+MDIO_TOOLS_VERSION = 1.1.0
 MDIO_TOOLS_SOURCE  = mdio-tools-$(MDIO_TOOLS_VERSION).tar.gz
 MDIO_TOOLS_SITE    = https://github.com/wkz/mdio-tools/releases/download/$(MDIO_TOOLS_VERSION)
 MDIO_TOOLS_LICENSE = GPL-2.0
