7c307110a8032ca22baa026a07b4c171e67756e6
Joacim Zetterling <joacim.zetterling@westermo.com>
2023-12-07 14:41:58 +0100

    dts: common: added index and speed parameters to mv6 struct
    Signed-off-by: Joacim Zetterling <joacim.zetterling@westermo.com>

--- dts/include/common/mv6.h
+++ dts/include/common/mv6.h
@@ -1,25 +1,28 @@
-
-#define _mv6_port_common(_reg, _label, _mode)		\
+#define _mv6_port_common(_index, _reg, _label, _mode)	\
 	reg = <_reg>;					\
 	label = _label;					\
 	local-mac-address = [00 00 00 00 00 00];	\
 	phy-mode = _mode
 
-#define mv6_port(_reg, _label, _phy, _mode)		 \
+#define mv6_port(_index, _reg, _label, _phy, _mode)	 \
 	port@_reg {					 \
 		phy-handle = <_phy>;			 \
-		_mv6_port_common(_reg, _label, _mode);	 \
+		_mv6_port_common(_index, _reg, _label, _mode);	\
 	}
 
-#define mv6_port_sfp(_reg, _label, _sfp, _mode)		 \
+#define mv6_label_port(_switch, _index, _reg, _label, _phy, _mode)	\
+	_switch##_port##_reg: mv6_port(_index, _reg, _label, _phy, _mode)
+
+#define mv6_port_sfp(_index, _reg, _label, _sfp, _mode)	 \
 	port@_reg {					 \
 		managed = "in-band-status";		 \
 		sfp = <_sfp>;				 \
-		_mv6_port_common(_reg, _label, _mode);	 \
+		_mv6_port_common(_index, _reg, _label, _mode);	 \
 	}
 
-#define mv6_phy(_reg)					\
+#define mv6_phy(_reg, _speed)				\
 	ethernet-phy@_reg {				\
 		reg = <_reg>;				\
 		interrupts = <_reg IRQ_TYPE_LEVEL_LOW>;	\
+		max-speed = <_speed>;			\
 	}
