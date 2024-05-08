3dca67199fe8ac1ff229f007c1e1d61b8909beba
Joacim Zetterling <joacim.zetterling@westermo.com>
2024-05-08 21:24:28 +0200

    package: mdnsd: finit: add missing mdnsd finit hook
    Signed-off-by: Joacim Zetterling <joacim.zetterling@westermo.com>

---
+++ package/mdnsd-finit/mdnsd-finit.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# mdnsd-finit
+#
+################################################################################
+
+MDNSD_FINIT_VERSION = 0.12
+MDNSD_FINIT_SITE =
+MDNSD_FINIT_SOURCE =
+MDNSD_FINIT_LICENSE = ISC
+MDNSD_FINIT_LICENSE_FILES = LICENSE
+MDNSD_FINIT_DEPENDENCIES = host-pkgconf avahi
+MDNSD_FINIT_INSTALL_STAGING = YES
+
+define MDNSD_FINIT_INSTALL_FINIT_SVC
+	@echo "#### MDNSD_INSTALL_FINIT_SVC"
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_NETBOX_PATH)/package/mdnsd-finit/mdnsd-finit.svc \
+		$(FINIT_D)/available/mdnsd.conf
+	$(INSTALL) -d -m 0755 $(FINIT_D)/enabled
+	ln -sf ../available/mdnsd.conf $(FINIT_D)/enabled/mdnsd.conf
+endef
+MDNSD_FINIT_POST_INSTALL_TARGET_HOOKS += MDNSD_FINIT_INSTALL_FINIT_SVC
+
+$(eval $(generic-package))
